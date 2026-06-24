b62666b3ed016fdb574af9e0c525b732c1a8b202
Ardonesa Maloku <ardonesa.maloku@uni-konstanz.de>
2026-06-24 20:28:41 +0200

    Fix order process and persistence

--- src/main/java/de/htwg/in/schneider/easygather/backend/config/SecurityConfig.java
+++ src/main/java/de/htwg/in/schneider/easygather/backend/config/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -14,6 +15,8 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http
+                .csrf(csrf -> csrf.disable())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .cors(withDefaults())
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers("/api/profile").authenticated()
